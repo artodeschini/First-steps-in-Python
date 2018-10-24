010db6ec8af89b6dee2bd129604719cdea742f14
Artur Todeschini <artodeschini@gmail.com>
2018-10-24 21:59:55 -0200

    alterado tabuada para usar range ao inves de list

--- tabuada_de_numero.py
+++ tabuada_de_numero.py
@@ -1,7 +1,5 @@
 print('Digite um numero de 1 a 10 ')
 n = int(input())
 
-numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-
-for i in numbers :
+for i in range(1,11) :
 	print('{0} * {1} = {2}'.format( n, i, ( n * i ) ) )
