9bb64f283fad0ea8ac2e46a6ea22fb6c821dfe7f
Artur Todeschini <artodeschini@gmail.com>
2018-10-26 20:44:14 -0200

    add files for new curse

---
+++ function_print.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+c = 3
+
+print(f'the value of a is {a}')
+
+print(f'the sum of {a} + {b} = {a + b} ')
---
+++ multiple_tables.py
@@ -0,0 +1,6 @@
+
+
+for n in range(1,11) :
+    print(f'A tabuada do número {n}' )
+    for i in range(1,11) :
+	    print(f' {n} * {i} = {n * i}' )
