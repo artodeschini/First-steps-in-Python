8eb872ffda2118d4f6662cba9a7e42bf8c60b0da
Artur Todeschini <artodeschini@gmail.com>
2018-10-27 21:03:50 -0200

    add files for new curse

---
+++ for_sample_best_pratic.py
@@ -0,0 +1,17 @@
+
+print('best more readable use two space or more')
+for i in range( 2, 5 ) :
+    print(i)
+print('no recomendation ')
+for i in range( 2, 5 ) : print(i)
+print('python recommends 4 spaces')
+for i in range(1,3) :
+    print('indentation is a scope, we not use { } to delimit it')
+
+# this show 2 4 6 7 8 10
+for i in range(2, 11, 2) :
+    print(i)
+
+# this show reverse
+for i in range(11,0, -1) :
+    print(i)
---
+++ learning_string.py
@@ -0,0 +1,6 @@
+
+old = 'Java'
+new = 'Python'
+
+print(f'Learning a new language {new}')
+print("{} It's much simpler than {} ".format( new , old) )
