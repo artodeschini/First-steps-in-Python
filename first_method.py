c07b398547fa61f663e2159ed9e2c1d36056ae48
Artur Todeschini <artodeschini@gmail.com>
2018-11-03 10:47:18 -0200

    learn about method in python

---
+++ first_method.py
@@ -0,0 +1,23 @@
+def print_hello_world_2x():
+    for i in range(0,2) :
+        print("Hello World")
+
+
+def say_hello_world_3x() :
+    for i in range(0, 3) :
+        print('Hello World')
+
+
+def i_have_learned_about() :
+    aList = ['variable', 'loop', 'method'];
+    for str in aList :
+        print("I've create my first " + str)
+    print("I've excited to learn Python")
+
+
+
+print_hello_world_2x()
+print()
+say_hello_world_3x()
+print()
+i_have_learned_about()
