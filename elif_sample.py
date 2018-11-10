85c7438da410c8b3a4a941e3b3eb2baf34792677
Artur Todeschini <artodeschini@gmail.com>
2018-11-10 18:30:29 -0200

    add note and samples to if elif else

---
+++ elif_sample.py
@@ -0,0 +1,17 @@
+print('Digite um numero');
+i = int(input())
+
+if i == 1 :
+    print(f'i is {i}')
+elif i == 2 :
+    print(f'i is {i}')
+elif i == 3 :
+    print(f'i is {i}')
+elif i == 4 :
+    print(f'i is {i}')
+elif i == 5 :
+    print(f'i is {i}')
+elif i == 6 :
+    print(f'i is {i}')
+else:
+    print(f'i is not in 1,2,3,4,5,6 i is {i}')
