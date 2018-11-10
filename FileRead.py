323fbb9bbe78a832136bee204d9fdcdbcd1b0f2f
Artur Todeschini <artodeschini@gmail.com>
2018-11-10 08:46:21 -0200

    write and read file

---
+++ FileRead.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+file = str(Path.home()) + "/python.txt"
+
+try :
+
+    with open(file) as f:
+        # the method read 'read de file' and splitlines separe by '\n'
+        content = f.read().splitlines()
+    print( content )
+except :
+    print('One error')
