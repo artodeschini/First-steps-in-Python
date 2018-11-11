0e556f24e8a1789ab8b27e312ff2fd4f70bb5b8f
Artur Todeschini <artodeschini@gmail.com>
2018-11-11 18:06:15 -0200

    no support to char

---
+++ no_support_to_char.py
@@ -0,0 +1,17 @@
+# python there is no support to character
+
+myText = 'Artur'
+
+aChar = myText[0]
+
+print( aChar )
+print( type(aChar ) )
+
+inverse = myText[::-1]
+print( inverse )
+
+# get length of str
+print( len(myText) )
+
+for c in myText :
+    print( c )
