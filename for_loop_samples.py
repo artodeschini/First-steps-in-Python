5070f9fb867bb203d1b1647921b3c47a80e82273
Artur Todeschini <artodeschini@gmail.com>
2018-11-15 20:24:46 -0200

    samples for loop

---
+++ for_loop_samples.py
@@ -0,0 +1,16 @@
+
+# print to 1 to 10
+for i in range(1,11):
+    print(i)
+
+# print character to character
+for c in 'Artur Todeschini Crestani':
+    print(c)
+
+# print word a word in a str
+for word in 'Artur Todeschini Crestani'.split():
+    print(word)
+
+# print number in
+for n in (2,6,9):
+    print(n)
