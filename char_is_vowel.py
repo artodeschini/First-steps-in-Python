edb25541a66785ef5d3a95d334111841f3217d06
Artur Todeschini <artodeschini@gmail.com>
2018-11-15 20:13:16 -0200

    samples with string

--- char_is_vowel.py
+++ char_is_vowel.py
@@ -1,4 +1,4 @@
-def is_vowel( c ) :
+def is_vowel(c) :
     if len(c) > 1 :
         return False
     elif c in 'AEIOUaeiou':
@@ -6,14 +6,29 @@ def is_vowel( c ) :
     else:
         return False
 
+def is_consonat(c) :
+    if len(c) > 1:
+        return False
+    elif c.isalpha() and not c in 'AEIOUaeiou':
+        return True
+    else:
+        return False
 
 char = input('type a character\n')
 message = f'{char} is'
-if is_vowel( char ) :
+if is_vowel(char) :
     message += ' a vowel'
 else:
     message += ' not a vowel'
 
 
+print(message)
 
+char = input('type a character\n')
+message = f'{char} is'
+if is_consonat(char) :
+    message += ' a consonant'
+else:
+    message += ' not a consonant'
 
+print(message)
