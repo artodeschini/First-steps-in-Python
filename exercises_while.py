2178b1c65124979b13333ca36896b54bae70c8ab
Artur Todeschini <artodeschini@gmail.com>
2018-11-17 21:10:48 -0200

    samples with for tasks

---
+++ exercises_while.py
@@ -0,0 +1,17 @@
+def print_squares_up_to(number):
+    i = 1
+    result = 0
+    while result < 30:
+        print(result)
+        result = i * i
+        i += 1
+def print_cubes_up_to(number):
+    i = 1
+    result = 0
+    while result < 30:
+        print(result)
+        result = pow(i,3)
+        i += 1
+
+print_squares_up_to(30)
+print_cubes_up_to(30)
