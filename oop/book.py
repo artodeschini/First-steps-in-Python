caaeb250aa05b378b3c310589bb4d7aae484ae2c
Artur Todeschini <artodeschini@gmail.com>
2018-11-24 19:11:00 -0200

    add constructor in book sample

--- oop/book.py
+++ oop/book.py
@@ -1,16 +1,21 @@
 # class Book
 class Book:
-    pass
+    def __init__(self, name):
+        self.name = name
 
-hf = Book()
-bible = Book()
-tp = Book()
+# hf = Book()
+# bible = Book()
+# tp = Book()
 
-hf.name = 'Head in First'
-bible.name  = 'Bible'
-tp.name  = 'Thinking in Python'
+#hf.name = 'Head in First'
+#bible.name  = 'Bible'
+#tp.name  = 'Thinking in Python'
+
+hf = Book('Head in First')
+bible = Book('Bible')
+tp = Book('Thinking in Python')
 
 books = { hf, bible, tp }
 
-for b in books:
-    print( b.name )
+for i,b in enumerate(books):
+    print( f'the {i + 1} book is {b.name} ')
