c740a56d0ab0f687e4ef0d60ca6cd7c653814a68
Artur Todeschini <artodeschini@gmail.com>
2018-11-26 20:33:28 -0200

    refactor class book

--- oop/book.py
+++ oop/book.py
@@ -1,7 +1,19 @@
 # class Book
 class Book:
-    def __init__(self, name):
+
+    #contrutor of Book
+    def __init__(self, name, copies=0):
         self.name = name
+        self.copies = copies
+
+    def increse_copies(self, how_much):
+        self.copies += how_much
+
+    def decrese_copies(self, how_much):
+        self.copies -= how_much
+
+    pass
+
 
 # hf = Book()
 # bible = Book()
@@ -11,11 +23,27 @@ class Book:
 #bible.name  = 'Bible'
 #tp.name  = 'Thinking in Python'
 
+
 hf = Book('Head in First')
-bible = Book('Bible')
-tp = Book('Thinking in Python')
+bible = Book('Bible', 12)
+tp = Book('Thinking in Python', 2)
 
-books = { hf, bible, tp }
+books = {hf, bible, tp}
 
-for i,b in enumerate(books):
-    print( f'the {i + 1} book is {b.name} ')
+
+def show_books():
+    for i,b in enumerate(books):
+        print(f'the {i + 1} book is {b.name} number of copies {b.copies}')
+
+
+show_books()
+
+for b in books:
+    b.increse_copies(15)
+
+show_books()
+
+for b in books:
+    b.decrese_copies(5)
+
+show_books()
