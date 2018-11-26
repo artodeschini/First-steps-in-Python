6214aed1d6828df52df261a6726cb5962c59a761
Artur Todeschini <artodeschini@gmail.com>
2018-11-26 20:46:08 -0200

    all is a object in python

---
+++ oop/everthing_is_object_in_python.py
@@ -0,0 +1,26 @@
+
+print(type(1))
+print(type(1.5))
+print(type('hello'))
+print(type(True))
+print('a text'.upper())
+
+
+def a_sample_function():
+    print('make anything')
+
+
+print(type(a_sample_function))
+
+# create a alias to a function or a instance
+sample = a_sample_function()
+
+print(type(sample))
+
+print(type(None))
+
+# call the function
+sample
+
+print("don't forget all in Python is a object")
+
