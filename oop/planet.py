089e3658a3db908a9ccda7dc26c727440ac51443
Artur Todeschini <artodeschini@gmail.com>
2018-11-26 20:33:59 -0200

    refactor class Planet

--- oop/planet.py
+++ oop/planet.py
@@ -1,4 +1,16 @@
 class Planet:
+
+    def rotate(self):
+        print('Rotate')
+
+    def revolve(self):
+        print('Revolve')
+
+    def rotate_and_revolve(self):
+        self.rotate()
+        self.revolve()
+
+
     pass
 
 
@@ -13,3 +25,4 @@ planets = {venus, earth}
 
 for i, p in enumerate(planets):
     print(f'The {i +1} planet is {p.name}')
+    p.rotate_and_revolve()
