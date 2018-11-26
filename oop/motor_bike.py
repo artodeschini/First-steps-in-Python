dd87c00941cc8578f46865726fa17296cfa14476
Artur Todeschini <artodeschini@gmail.com>
2018-11-26 20:10:44 -0200

    modify class MotorDike

--- oop/motor_bike.py
+++ oop/motor_bike.py
@@ -1,18 +1,48 @@
 # class Motor Bike
 
+
 class MotorBike:
+
     def __init__(self, name, spped):
         self.name = name
         self.spped = spped
 
-honda  = MotorBike('Honda', 80)
+    def increse_speed(self, how_much):
+        self.spped += how_much
+
+    def decrese_speed(self, how_much):
+        self.spped += how_much
+
+    pass
+
+
+honda = MotorBike('Honda', 80)
 ducati = MotorBike('Ducati', 250)
 
-motors = ( honda, ducati )
 
-for m in motors:
-    print(m.name)
-    print(m.spped)
+motors = (honda, ducati)
+
+
+def show_motors():
+    for m in motors:
+        print(m.name)
+        print(m.spped)
+
+
+show_motors()
+
+
+honda.increse_speed(160)
+ducati.increse_speed(200)
+
+show_motors()
+
+honda.decrese_speed(29)
+ducati.decrese_speed(38)
+
+show_motors()
+
+
 
 #honda.spped = 80
 #ducati.spped = 250
