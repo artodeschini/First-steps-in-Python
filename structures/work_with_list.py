72f6bd598dec957329f4a4a9ecd3ab0822797260
Artur Todeschini <artodeschini@gmail.com>
2018-11-27 21:18:58 -0200

    add structure list sample

--- structures/work_with_list.py
+++ structures/work_with_list.py
@@ -1,6 +1,12 @@
 # a sample of list
+# to create a list in python is very simple
+# put values in square bracket start '[' and end ']'
 makes = [10, 20, 30]
 print(type(makes))
+
+
+def show_index_of_a_element(element):
+    print('The index of 30 is {0}'.format(makes.index(element)))
 
 
 def calcule_makes_avg():
@@ -14,9 +20,31 @@ def show_makes():
 
 show_makes()
 print(calcule_makes_avg())
+# show the max value
+print(max(makes))
+# show the min value
+print(min(makes))
+print('The index of 30 is {0}'.format(makes.index(30)))
 
 print('Add one more in makes ---> 40')
 makes.append(40)
 
 show_makes()
 print(calcule_makes_avg())
+# show the max value
+print(max(makes))
+# show the min value
+print(min(makes))
+print(len(makes))
+
+makes.insert(1, 15)
+print('The index of 30 is {0}'.format(makes.index(30)))
+show_index_of_a_element(30)
+print(len(makes))
+
+show_makes()
+
+makes.remove(15)
+print('The index of 30 is {0}'.format(makes.index(30)))
+show_index_of_a_element(30)
+print(len(makes))
