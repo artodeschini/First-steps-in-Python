49ff0b2b9090b6b85886d501bf2f93abff70af43
Artur Todeschini <artodeschini@gmail.com>
2018-11-28 23:07:24 -0200

    sort a list of objects with a attribute get min and max object with a attribute

--- structures/country_work_with_list.py
+++ structures/country_work_with_list.py
@@ -1,3 +1,6 @@
+from operator import attrgetter
+
+
 class Country:
 
     # this is a constructor of the class
@@ -11,16 +14,31 @@ class Country:
         return repr((self.name, self.population, self.area))
 
 
+uk = Country('United Kingdon', 500, 90)
 india = Country('India', 1200, 100)
 china = Country('China', 1400, 200)
 eua = Country('United States', 120, 300)
 
-countries = [india, china, eua]
+countries = [uk, india, china, eua]
 
 print(countries)
 
 countries.append(Country('Russia', 80, 500))
 
 print(countries)
-
 print(countries[len(countries):0: -1])
+
+# sort with a property
+countries.sort(key=attrgetter('name'))
+print(countries)
+
+# sort with a property reverse mode
+# sort with a property
+countries.sort(key=attrgetter('name'), reverse=True)
+print(countries)
+
+# get a country that has more population
+print(max(countries, key=attrgetter('population')))
+
+# get a country that has less area
+print(min(countries, key=attrgetter('area')))
