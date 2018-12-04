5f47974bc1fae898fd7791e69686306efd089eea
Artur Todeschini <artodeschini@gmail.com>
2018-12-04 21:07:03 -0200

    try except basic multiple except

--- exception_handler_error/exception_handling_basic.py
+++ exception_handler_error/exception_handling_basic.py
@@ -2,8 +2,16 @@ try:
     # digite a number != of 0 and a char
     i = int(input('Digite a number'))
     j = 10/i
-except:
+    values = [1, '1']
+    sum(values)
+
+except ZeroDivisionError:
+    print('ZeroDivisionError')
     j = 0
+
+except TypeError:
+    print('TypeError')
+
 
 print(j)
 print('End')
