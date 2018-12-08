dc3f5c10a731f08f6fd1085ff8779772c7bf6052
Artur Todeschini <artodeschini@gmail.com>
2018-12-08 12:33:12 -0200

    another types in python

---
+++ other_types_in_python/work_whit_decimal.py
@@ -0,0 +1,18 @@
+import decimal
+from decimal import Decimal
+import math
+
+value1 = Decimal('32.1')
+value2 = Decimal('42.1')
+result = value2 - value1
+
+print(result)
+print(type(result))
+
+print(math.pi)
+print(type(math.pi))
+
+print(math.e)
+print(type(math.e))
+
+print(math.factorial(5))
